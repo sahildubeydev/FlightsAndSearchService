f433a29ff4bf8e6d2efffff3f57101d4106fe3fa
Sahil Dubey <dsahil.contact@gmail.com>
2023-02-22 16:41:09 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -48,7 +48,7 @@ class CityRepository {
   async getCity(cityId) {
     try {
       const city = await City.findByPk(cityId);
-      return City;
+      return city;
     } catch (error) {
       console.log("Something went wrong in the repository layer under getCity");
       throw { error };

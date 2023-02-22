7d6a190826585ac0fb27c6263d340bb56edb495f
Sahil Dubey <dsahil.contact@gmail.com>
2023-02-22 16:39:30 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,13 +1,15 @@
 const { City } = require("../models/index");
 
 class CityRepository {
-
   // creating city.
   async createCity({ name }) {
     try {
       const city = await City.create({ name });
       return city;
     } catch (error) {
+      console.log(
+        "Something went wrong in the repository layer under creatCity"
+      );
       throw { error };
     }
   }
@@ -20,7 +22,35 @@ class CityRepository {
           id: cityId,
         },
       });
+      return true;
     } catch (error) {
+      console.log(
+        "Something went wrong in the repository layer under deleteCity"
+      );
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return City;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer under getCity");
       throw { error };
     }
   }

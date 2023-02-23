b23603af74535ddb41a66e2893fd8dd1e84623b5
Sahil Dubey <dsahil.contact@gmail.com>
2023-02-23 23:53:57 +0530

    Added airport seeders

--- README.md
+++ README.md
@@ -55,3 +55,8 @@ repository/ - tests/[later] - static/ - temp/
 ### City -> id, name, created_at, updated_at
 ### Airport -> id, name, address, city_id, created_at, updated_at
     Relationship -> City has many airports and Airport belongs to a city (one to many)
+
+```
+npx sequelize model:generate --name Airport --attributes
+name:String,address:String,cityId:integer
+```
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 const ApiRoutes = require("./routes/index");
 
+const db = require("./models/index");
+// const { City, Airport } = require("./models/index");
+
 const setupAndStartServer = async () => {
   // create the express object
   const app = express();
@@ -15,6 +18,26 @@ const setupAndStartServer = async () => {
 
   app.listen(PORT, async () => {
     console.log(`Server started at ${PORT}`);
+    if (process.env.SYNC_DB) {
+      db.sequelize.sync({ alter: true });
+    }
+
+    // const city = await City.findOne({
+    //   where: {
+    //     id: 68,
+    //   },
+    // });
+    // const airports = await city.getAirports();
+    // const newAirport = await Airport.findOne({
+    //   where: {
+    //     id: 21,
+    //   },
+    // });
+    // await city.addAirport(newAirport);
+    // await city.addAirport({
+    //   name: "Jindal Vijaynagar Airport",
+    // });
+    // console.log(city, airports);
   });
 };
 
